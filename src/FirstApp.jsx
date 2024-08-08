9116306ae060619b4aceea985146b337155e5976
yuritzicalixto <ramirezcalixtodiana@gmail.com>
2024-08-07 19:59:22 -0600

    Impresión de variables

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+// import {Fragment} from 'react'
+const newMessage = {
+  message: 'Hola Mundo',
+  title: 'Diana'
+};
+
 
 export const FirstApp = () => {
   return (
-    <h1>Me aburro!!!!!!</h1>
+    // Nodo padre
+    /*Sinonimo de un fragmento
+    Es un agrupador de otros elementos html
+    En donde tenemos el nodo padre*/
+    <>
+          <h2> { newMessage.title }</h2>
+        {/* <h2> { newMessage.title }</h2> */}
+        {/* <code>  { JSON.stringify (newMessage)}</code> */}
+        <h1>Me aburro!!!!!!</h1>
+        <p>ejnkjnekj</p>
+    </>
   );
 }
