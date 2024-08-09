c333d598a59447505606f1bc5fe23aea0f6a0201
yuritzicalixto <ramirezcalixtodiana@gmail.com>
2024-08-08 21:59:34 -0600

    Cambio 1/2

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,22 +1,32 @@
 // import {Fragment} from 'react'
-const newMessage = {
-  message: 'Hola Mundo',
-  title: 'Diana'
-};
+// const newMessage = {
+//   message: 'Hola Mundo',
+//   title: 'Diana'
+// };
+
+ const getResult = () => {
+   return 4+4;
+ }
+
+const getResult2 = (a,b) => {
+  return a + b;
+}
 
 
 export const FirstApp = () => {
+  
   return (
+    
     // Nodo padre
     /*Sinonimo de un fragmento
     Es un agrupador de otros elementos html
     En donde tenemos el nodo padre*/
     <>
-          <h2> { newMessage.title }</h2>
+          {/* <h2> { newMessage.title }</h2> */}
         {/* <h2> { newMessage.title }</h2> */}
         {/* <code>  { JSON.stringify (newMessage)}</code> */}
         <h1>Me aburro!!!!!!</h1>
-        <p>ejnkjnekj</p>
+        <p> { getResult(1,2) } </p>
     </>
   );
 }
