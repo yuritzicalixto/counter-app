09c5476b72d793dcf7ad8898c3c8cd7e9d5c7552
yuritzicalixto <ramirezcalixtodiana@gmail.com>
2024-08-20 17:37:02 -0600

    CounterApp-Homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HelloWorldApp} from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
-
+// import {HelloWorldApp} from './HelloWorldApp';
+// import { FirstApp } from './FirstApp';
 import './styles.css'
+import { CounterApp } from './CounterApp';
 
 // export default App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title/>
+        <CounterApp value={123} />
     </React.StrictMode>
 );
