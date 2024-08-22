77ea5e20ca20a79f8d3c5c20753c66a264744c5f
yuritzicalixto <ramirezcalixtodiana@gmail.com>
2024-08-21 20:34:20 -0600

    Expresiones de JS-1/3

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,7 +4,12 @@ export const CounterApp = ({value}) => {
     return (
         <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2>{ value }</h2>
+
+        <button onClick={ function(){console.log('+1');
+        } }>
+            +1
+        </button>
         </>
     );
 }
